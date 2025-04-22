05d6295cefd3ae99eef0b03c20a3d3d53e6fa9b0
Shashwat Jain <kcapshashwat@gmail.com>
2025-04-22 15:40:45 +0530

    Refactor code

--- components/dashboard-header.tsx
+++ components/dashboard-header.tsx
@@ -1,6 +1,5 @@
-import type React from "react"
 import { cn } from "@/lib/utils"
-import { ModelViewer } from "./3d-model-viewer"
+import type React from "react"
 
 interface DashboardHeaderProps {
   heading: string
@@ -14,7 +13,7 @@ export function DashboardHeader({ heading, text, children, className }: Dashboar
     <div className={cn("flex flex-col gap-2 md:flex-row md:items-center md:justify-between", className)}>
       <div className="space-y-0.5 flex items-center gap-4 mb-10">
         <h1 className="text-4xl font-bold tracking-tight ">{heading}</h1>
-        <ModelViewer className="hidden md:block" />
+        {/* <ModelViewer className="hidden md:block" /> */}
         {text && <p className="text-muted-foreground">{text}</p>}
       </div>
       {children}
